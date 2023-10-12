fdc60f363ba58044c735ac0e112debbcb17d6481
Andrey Gudin <aeon150791@gmail.com>
2023-10-12 15:49:38 +0400

    feat: add create route folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Global, Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { CreateModule } from './create/create.module';
 
 @Global()
 @Module({
-  imports: [HttpModule, ConfigModule.forRoot(), AuthModule],
+  imports: [HttpModule, ConfigModule.forRoot(), AuthModule, CreateModule],
   exports: [HttpModule],
 })
 export class AppModule {}

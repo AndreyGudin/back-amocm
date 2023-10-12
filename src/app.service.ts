b3f14ef1f3e7a98fa34f941498ce112ff3fe370e
Andrey Gudin <aeon150791@gmail.com>
2023-10-12 15:31:42 +0400

    feat: add auth route

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { AxiosResponse } from 'axios';
+import { Observable, map } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly httpService: HttpService) {}
+  auth(): Observable<
+    AxiosResponse<{ access_token: string; base_domain: string }>
+  > {
+    return this.httpService
+      .get('https://test.gnzs.ru/oauth/get-token.php', {
+        headers: {
+          'X-Client-Id': JSON.stringify(process.env.X_CLIENT_ID),
+        },
+      })
+      .pipe(map((resp) => resp.data));
   }
 }

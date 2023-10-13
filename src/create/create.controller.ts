546bcc271ff2307a207d91e85bff5fdab619d35d
Andrey Gudin <aeon150791@gmail.com>
2023-10-13 10:56:48 +0400

    feat: add routes to controller

--- src/create/create.controller.ts
+++ src/create/create.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, NotFoundException, Post, Req } from '@nestjs/common';
 import { CreateService } from 'src/create/create.service';
+import { CreateCompanyDto } from 'src/create/dto/create-company.dto copy';
+import { CreateContactDto } from 'src/create/dto/create-contact.dto';
 import { CreateLeadDto } from 'src/create/dto/create-lead.dto';
 
 @Controller('create')
@@ -9,6 +11,24 @@ export class CreateController {
   createLead(@Req() request: Request, @Body() createLeadDto: CreateLeadDto) {
     const token = request.headers.get('Authorization: Bearer');
     if (token) return this.createService.createLead(createLeadDto, token);
+    throw new NotFoundException();
+  }
+
+  createContact(
+    @Req() request: Request,
+    @Body() createContactDto: CreateContactDto,
+  ) {
+    const token = request.headers.get('Authorization: Bearer');
+    if (token) return this.createService.createContact(createContactDto, token);
+    throw new NotFoundException();
+  }
+
+  createCompany(
+    @Req() request: Request,
+    @Body() createCompanyDto: CreateCompanyDto,
+  ) {
+    const token = request.headers.get('Authorization: Bearer');
+    if (token) return this.createService.createCompany(createCompanyDto, token);
     throw new NotFoundException();
   }
 }
